d9a2c12463d466227c347ed39bead062a5e8316f
Jakob <jakob@localhost.localdomain>
2019-11-21 12:27:48 +0100

    Added utils

---
+++ src/eos/utils.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jakob on 09.10.19.
+//
+
+#include "utils.hpp"
---
+++ src/eos/utils.hpp
@@ -0,0 +1,14 @@
+//
+// Created by jakob on 09.10.19.
+//
+
+#ifndef GLFW_GAMEENGINE_UTILS_HPP
+#define GLFW_GAMEENGINE_UTILS_HPP
+
+
+class utils {
+
+};
+
+
+#endif //GLFW_GAMEENGINE_UTILS_HPP
